da8217e6deea2a2ba9c4d5d2be06dd639388f4c4
Andrew Mannion <andrew.mannion@ucdconnect.ie>
2019-04-04 10:21:53 +0100

    SPI master functionality, testbenches to be fleshed out later

--- verilog/design_sources/AHBspi.v
+++ verilog/design_sources/AHBspi.v
@@ -13,7 +13,26 @@ module AHBspi (
     output wire HREADYOUT
 )
 
+    wire spi_ready_x;
     reg h_write_r;
+    
+    reg[31:0] ctrl_status_r, spi_ss_r, write_only_r, read_only_r;
+    
+    
+    // If HSEL = 1, HWRITE = 1, and HREADY = 1, then allow write into control/ status 
+    // registers as the previous transaction is about to complete
+    always @(posedge HCLK) begin
+        if(HSEL && HREADY) begin
+            // Capture HADDR, HWRITE
+            HADDR_r  <= HADDR;
+            HWRITE_r <= HWRITE;
+        end    
+    
+        if(~HRESETn) begin
+            HADDR_r  <= 32'b0;
+            HWRITE_r <= 1'b0;
+        end
+    end
 
     // Capture next command for pipelining
     always @ (posedge HCLK) begin
@@ -24,6 +43,33 @@ module AHBspi (
         end    
     end
     
+    // Control/status register
+    //                      CONTROL
+    // | CPOL | CPHA | SPI_TRANSACTION_SIZE? |  -  | -  | -  |
+    // |  31  |  30  |      29,28,27,26      |  25 | 24 | 23 |
+    //
+    //                  STATUS
+    // | WRITE_REG_STATUS | READ_REG_STATUS | 
+    // |      22,21,20    |     19,18,17     | 16 | 15 |
+    // 
+    always @() begin
     
+    
+    end
+    
+    // Address phase
+    
+    
+    SPIMaster spiMaster (
+    .clk_i(HCLK),
+    .rstn_i(HRESETn),
+    .ready_i(),
+    .spi_tx_data_i(),
+    .spi_miso_i(),
+    .spi_mosi_o(),
+    .spi_clk_o(),
+    .spi_ss_o(),
+    .ready_o(spi_ready_x)
+);
 
 endmodule
--- verilog/design_sources/SPImaster.v
+++ verilog/design_sources/SPImaster.v
@@ -1,45 +1,57 @@
-module SPImaster (
-    input wire clk_i,
-    input wire rstn_i,
-    input wire rw_i,
-    input wire ready_i,
-    inout wire[7:0] spi_data_io,
-    input wire spi_miso_i,
-    output wire spi_mosi_o,
-    output wire spi_clk_o,
-    output wire spi_ss_o,
-    output wire ready_o
-)
+module SPIMaster (
+    input wire       clk_i,
+    input wire       rstn_i,
+    input wire       rw_i,
+    input wire       ready_i,
+    input wire [7:0] spi_tx_data_i,
+    input wire       spi_miso_i,
+    output wire      spi_mosi_o,
+    output wire      spi_clk_o,
+    output wire      spi_ss_o,
+    output wire      ready_o
+);
 
-    wire spi_clk_x, load_en_x;
-    wire[1:0] shift_enable_x;    
-
-    SPIwrite_shift_reg spi_write_shift_reg(
-        .spi_clk_i(spi_clk_x),
+    wire load_clk_x, load_en_x;
+    wire shift_enable_x; 
+    
+    SPIShiftReg #(
+        .RWn(1)
+    )
+    spiReadShiftReg
+    (
+        .clk_i(load_clk_x),
         .rstn_i(rstn_i),
-        .data_i(spi_data_i),
-        .load_data_en_i(load_en_x)
+        .data_bit_i(spi_miso_i),
+        .data_i(8'd0),
+        .load_data_en_i(load_en_x),
+        .shift_en_i(shift_enable_x),
         .shift_out_o(spi_mosi_o)
     );
     
-    SPIread_shift_reg spi_read_shift_reg(
-        
+    SPIShiftReg #(
+        .RWn(0)    
+    )
+    spiWriteShiftReg (
+        .clk_i(load_clk_x),
+        .rstn_i(rstn_i),
+        .data_bit_i(1'b0),
+        .data_i(spi_tx_data_i),
+        .load_data_en_i(load_en_x),
+        .shift_en_i(shift_enable_x),
+        .shift_out_o(spi_mosi_o)
     );
     
-    
-    
     // Tells shift register whether reading or writing
-    SPImaster_control spi_master_control(
+    SPIMasterControl spiMasterControl(
         .clk_i(clk_i),
         .rstn_i(rstn_i),
-        .rw_i(),                            // Indicate type of transaction
-        .ready_i(enable_i),                 // Request a transaction 
-        .spi_data_io(spi_data_io),          // Provide data for writing
-        .load_data_en_o(load_en_x),         // Tell write shift reg to capture 8 bits of data to be sent
-        .read_shift_enable_o(read_shift_enable_x),  
-        .write_shift_enable_o(write_shift_enable_x),
-        .spi_clk_o(spi_clk_x),              
-        .ready_o(ready_o)                   // Indicate if SPI master is ready to accept a transaction
+        .rw_i(rw_i),                       // Indicate type of transaction
+        .ready_i(ready_i),                 // Request a transaction 
+        .load_data_en_o(load_en_x),        // Tell write shift reg to capture 8 bits of data to be sent
+        .shift_enable_o(shift_enable_x),
+        .load_clk_o(load_clk_x),
+        .spi_clk_o(spi_clk_o),              
+        .ready_o(ready_o)                  // Indicate if SPI master is ready to accept a transaction
     );
 
 endmodule
